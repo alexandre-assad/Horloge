934f3ad3b0204bda5c757b6b8a282d62a844b67f
yayassadou <alexandre.assad@laplateforme.io>
2023-03-16 15:17:51 +0100

    Reglages bug

--- horloge.py
+++ horloge.py
@@ -6,26 +6,35 @@ from tkinter.messagebox import *
 from tkinter import filedialog as fd
 
 
-h = 16
-m = 30
-s = 0
+h = 10
+m = 10
+s = 10
 horloge = (h,m,s)
 alarme = (-1,-1,-1)
 def afficher_heure(heure,mode):
     global horloge,h,m,s,Heure_entry
-    if type(heure) != tuple:
-        heure = list(heure.split(","))
-    h = int(heure[0])
-    m = int(heure[1])
-    s = int(heure [2])
-    horloge = (h,m,s)
-    if mode == "12H":
-        if h > 12:
-            return "{}:{}:{} PM".format(h-12,m,s)
+    try:
+        if type(heure) != tuple:
+            heure = list(heure.split(","))
+        h = int(heure[0])
+        m = int(heure[1])
+        s = int(heure [2])
+        horloge = (h,m,s)
+        if mode == "12H":
+            if h > 12:
+                return "{:02d}:{:02d}:{:02d} PM".format(h-12,m,s)
+            else:
+                return "{:02d}:{:02d}:{:02d} AM".format(h,m,s)
         else:
-            return "{}:{}:{} AM".format(h,m,s)
-    else:
-        return "{}:{}:{}".format(h,m,s)
+            return "{:02d}:{:02d}:{:02d}".format(h,m,s)
+    except:
+        if mode == "12H":
+            if h > 12:
+                return "{:02d}:{:02d}:{:02d} PM".format(h-12,m,s)
+            else:
+                return "{:02d}:{:02d}:{:02d} AM".format(h,m,s)
+        else:
+            return "{:02d}:{:02d}:{:02d}".format(h,m,s)
 def changemod():
     global mode
     if mode == "24H":
@@ -36,27 +45,34 @@ async def tick():
     global horloge, h, m, s
     await asyncio.sleep(0.1)
     s+=1
-    if s > 60:
-        s = 0
-        m += 1
-        if m > 60:
-            m = 0
-            h+=1
-            if h > 24:
-                h = 0
-    horloge = (h,m,s)
-    return horloge
+    if s <= 60 and m <= 60 and h <= 24:
+        if s > 60:
+            s = 0
+            m += 1
+            if m > 60:
+                m = 0
+                h+=1
+                if h > 24:
+                    h = 0
+        horloge = (h,m,s)
+        return horloge
+    else:
+        horloge = (0,0,0)
+        return horloge
 
 def regler_alarme(heure):
     global horloge,alarme,Alarme_entry
-    if type(heure) != tuple:
-        heure = list(heure.split(","))
-        h = int(heure[0])
-        m = int(heure[1])
-        s = int(heure [2])
-    alarme = (h,m,s)
-    Alarme_entry.delete(0,END)
-    return alarme
+    try:
+        if type(heure) != tuple:
+            heure = list(heure.split(","))
+            h = int(heure[0])
+            m = int(heure[1])
+            s = int(heure [2])
+        alarme = (h,m,s)
+        Alarme_entry.delete(0,END)
+        return alarme
+    except:
+        return alarme
     
 def pause():
     global pause_state
@@ -73,7 +89,6 @@ def config_heure():
     global horloge, heureText,alarme,alarmeText,pause_state
     if pause_state != "pause":
         horloge = asyncio.run(tick())
-        print("a")
     if horloge == alarme:
         alarmeText.config(fg="blue")
         alarme = (-1,-1,-1)
@@ -98,13 +113,13 @@ AfficherText = Label(frame, text="L'heure Actuelle :",background="grey",font=("A
 AfficherText.grid(column=0,row=0)
 heureText = Label(frame, text=str(afficher_heure(horloge,mode)),background="grey",font=("Arial",20))
 heureText.grid(column=1,row=0)
-ReglerHeureText = Label(frame, text="Regler l'heure :",background="grey",font=("Arial",20))
+ReglerHeureText = Label(frame, text="Regler l'heure (en24H et h,m,s):",background="grey",font=("Arial",20))
 ReglerHeureText.grid(column=0,row=1)
 Heure_entry= Entry(frame)
 Heure_entry.grid(column=1,row=1)
-ReglerHeureButton = Button(frame, text="Regler l'heure",font=("Arial",10),width=9, background="Blue",fg="white",command=lambda :clearheure())
+ReglerHeureButton = Button(frame, text="Regler l'heure ",font=("Arial",10),width=9, background="Blue",fg="white",command=lambda :clearheure())
 ReglerHeureButton.grid(column=0,row=2)
-ReglerAlarmeText = Label(frame, text="Mettre une alarme :",background="grey",font=("Arial",20))
+ReglerAlarmeText = Label(frame, text="Mettre une alarme (en24H et h,m,s):",background="grey",font=("Arial",20))
 ReglerAlarmeText.grid(column=0,row=3)
 Alarme_entry= Entry(frame)
 Alarme_entry.grid(column=1,row=3)
@@ -122,43 +137,3 @@ alarmeText = Label(frame,text="L'ALARME SONNE... DING DONG",fg="grey",background
 alarmeText.grid(column=0,row=7)
 config_heure()
 fenetre.mainloop()
-
-    
-        
-# while True:
-#     alarme = (-1,-1,-1)
-#     pause_state = ""
-#     mode = "24H"
-#     action = int(input("""Que Voulez Vous faire ?
-#     (0) Afficher l'heure
-#     (1) Regler l'heure
-#     (2) Mettre une alarme 
-#     (3) Changer le mode 12H/24H
-#     (4) Mettre en pause/depause
-#     (5) Quitter                           
-    
-#     """))
-#     if action == 0:
-#         afficher_heure(horloge,mode)
-#     elif action == 1:
-#         newheure = tuple(input("Quel heure vous voulez, écrire de la forme (h,m,s)"))
-#         afficher_heure(newheure,mode)
-#     elif action == 2:
-#         alarme = tuple(input("Quel heure pour l'alarme vous voulez, écrire de la forme (h,m,s)"))
-        
-#     elif action == 3:
-#         if mode == "24H":
-#             mode = "12H"
-#         else:
-#             mode = "24H"
-#     elif action == 4:
-#         if pause_state == "pause":
-#             pause_state = ""
-#         else:
-#             pause_state = "pause"
-#     elif action == 5:
-#         break
-#     if pause(pause_state) != True:
-#         horloge = asyncio.run(tick())
-#         afficher_heure(horloge,mode)
-#     regler_alarme(alarme)
